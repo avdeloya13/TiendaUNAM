8df10380d5ccf3290bc9a7a21885b7317dbbdcab
Valeria Deloya <anavaleriadeloya@ciencias.unam.mx>
2024-11-07 23:07:10 -0600

    feature sprint 1

--- TiendaOnlineUNAM/src/app/modules/product/component/by-category/by-category.component.ts
+++ TiendaOnlineUNAM/src/app/modules/product/component/by-category/by-category.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { ByCategoryService } from '../../_service/by-category.service';
 import { SwalMessages } from '../../../../shared/swal-messages';
 import { ProductService } from '../../_service/product.service';
-import { CategoryService } from '../../_service/category.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { SharedModule } from '../../../../shared/shared-module';
+import { ProductImageService } from '../../_service/product-image.service';
 
 @Component({
   selector: 'app-by-category',
@@ -21,18 +20,40 @@ export class ByCategoryComponent {
 
   id: any = '';
 
+  productImgs: any[] = [];
+
   constructor(private productService: ProductService,
+              private productimageService: ProductImageService,
               private route: ActivatedRoute,
-              private router: Router,
-              private categoryService: CategoryService){ }
+              private router: Router){ }
 
   ngOnInit():void{
     this.route.paramMap.subscribe((params) => {
       this.id = params.get('id');
       if (this.id) {
         this.getProductsbyCategory();
+        this.getProductImage();
       }
     });
+  }
+
+  getProductImage(){
+    this.productimageService.getProductImage(this.id).subscribe({
+      next: (v) => {
+        console.log("Imágenes de productos:", v);
+        this.productImgs = v;
+      },
+      error: (e) => {
+        this.swal.errorMessage(e.error.message!);
+      }
+    });
+  }
+
+  getProductImageById(productId: number): string {
+    
+    const productImages = this.productImgs.filter(img => img.product_id === productId);
+
+    return productImages.length > 0 ? productImages[0].image : 'assets/no-product-image.png';
   }
 
   getProductsbyCategory(){
